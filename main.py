d10fe5abbfa6a472b4495baf04673ec701e748a1
sbarcoe90 <sbarcoe@outlook.ie>
2026-03-22 12:44:16 +0000

    Refactor main function to calculate and display word count from book text

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()   
 
+def get_num_words(book_text):
+    return len(book_text.split())
+    
 def main():
     book_path = "books/frankenstein.txt"
-    book1 = get_book_text(book_path)
-    print(book1)
+    book_text = get_book_text(book_path)
+    num_words = get_num_words(book_text)
+
+    print(f"Found {num_words} total words")
 
 main()
