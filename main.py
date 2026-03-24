2af5b7ea0137ae67335cd96e0a2f810ad0bfe2cf
sbarcoe90 <sbarcoe@outlook.ie>
2026-03-24 07:40:36 +0000

    Add get_num_chars function to stats.py and update main.py to display character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words
+from stats import get_num_words, get_num_chars
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -8,7 +8,9 @@ def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     num_words = get_num_words(book_text)
+    num_chars = get_num_chars(book_text)
 
     print(f"Found {num_words} total words")
-
+    print(f"Found {num_chars} total chars")
+    
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,17 @@
 def get_num_words(book_text):
     return len(book_text.split())
+
+def get_num_chars(book_text):
+    dict = {}
+    book_text_lower = book_text.lower()
+     # for each char, count +=1
+     # Place it in dict
+    count = 0
+    for c in book_text_lower:
+        if c in book_text_lower:
+            count += 1
+            dict = {c, count}
+            print(f"dict:: {dict}")
+        else:
+            pass
+    return dict
