70a69bce43246f2c34c88cae5d7ccec3a75bae19
sbarcoe90 <sbarcoe@outlook.ie>
2026-03-24 10:03:37 +0000

    Refactor get_num_chars function in stats.py to improve character counting logic

--- stats.py
+++ stats.py
@@ -2,16 +2,14 @@ def get_num_words(book_text):
     return len(book_text.split())
 
 def get_num_chars(book_text):
-    dict = {}
+    my_dict = {}
     book_text_lower = book_text.lower()
-     # for each char, count +=1
-     # Place it in dict
-    count = 0
-    for c in book_text_lower:
-        if c in book_text_lower:
-            count += 1
-            dict = {c, count}
-            print(f"dict:: {dict}")
+
+    for char in book_text_lower:
+
+        if char in my_dict:
+            my_dict[char] += 1
         else:
-            pass
-    return dict
+            my_dict[char] = 1   
+    
+    return my_dict
